afb9d4f45b74cd963fae137c82e32d95c4ef8bbb
ShaneDsouza4 <shane.json96@gmail.com>
2025-05-26 13:51:49 -0400

    Creating a Signal for Project Theme Color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ng-signals-example';
+
+  //Create signal
+  themeColor = signal('light');
+
+  ngOnInit(): void {
+    //Fetch value from signal
+    document.body.className = this.themeColor();
+  }
 }
